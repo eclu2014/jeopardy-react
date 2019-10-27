4e000a53c87d41aa507b627d968e0ffb3404e9a7
Emily Lu <eclu2014@gmail.com>
2019-10-27 04:17:02 -0400

    Updates

---
+++ precache-manifest.6ee542af169af5f1e1a06e46e57e9a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262af334986121588d17d54377786b37",
+    "url": "/jeopardy-react/index.html"
+  },
+  {
+    "revision": "4a69965ab079374a494d",
+    "url": "/jeopardy-react/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "234dd830050a5af8ebfb",
+    "url": "/jeopardy-react/static/css/main.11ff0c6c.chunk.css"
+  },
+  {
+    "revision": "4a69965ab079374a494d",
+    "url": "/jeopardy-react/static/js/2.f95feb9d.chunk.js"
+  },
+  {
+    "revision": "234dd830050a5af8ebfb",
+    "url": "/jeopardy-react/static/js/main.3294cf32.chunk.js"
+  },
+  {
+    "revision": "828829dfe01d7d331c6a",
+    "url": "/jeopardy-react/static/js/runtime-main.d9afe708.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jeopardy-react/static/media/logo.25bf045c.svg"
+  }
+]);
